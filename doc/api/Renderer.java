8093e799834b7f08bd6529e4e4a2a949b0013a41
Ben Xu <benjamin.xu@duke.edu>
2018-11-08 14:27:51 -0500

    started adding documentation to renderer

--- doc/api/Renderer.java
+++ doc/api/Renderer.java
@@ -4,6 +4,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
@@ -15,20 +16,51 @@ import javafx.stage.FileChooser;
  *  @author Orgil Batzaya */
 public interface Renderer{
 
-    public Button makeStringButton(String text);
+    /** Creates a {@code Button} with a label
+     *  @param text The label for the button
+     *  @param buttonColor The fill {@code Color} for the button
+     *  @param textColor The fill {@code Color} for the label
+     *  @param x The x position of the button
+     *  @param y The y position of the button */
+    public Button makeStringButton(String text, Color buttonColor, Color textColor, Double x, Double y);
 
-    public Button makeImageButton(Image image);
+    /** Creates a button using an image
+     *  @param image The {@code ImageView} that will be used as the button
+     *  @param x The x position of the button
+     *  @param y The y position of the button */
+    public Button makeImageButton(Image image, Double x, Double y);
 
+    /** Creates emphatic text
+     *  @param text The text to display
+     *  @param fontsize The size of the font
+     *  @param color The fill {@code Color} for the font
+     *  @param x The x position of the text
+     *  @param y The y position of the text*/
     public Text makeEmphasisText(String text, Integer fontsize, Color color, Double x, Double y);
 
+    /** Creates normal text
+     *  @param text The text to display
+     *  @param fontsize The size of the font
+     *  @param color The fill {@code Color} for the font
+     *  @param x The x position of the text
+     *  @param y The y position of the text*/
     public Text makePlainText(String text, Integer fontsize, Color color, Double x, Double y);
 
-    public ScrollPane makeGridScrollPane(List<Block> blockList);
+    /** Creates a scrollable Pane that displays its contents in a grid
+     *  @param blockList The content list, contains {@code ImageView} objects to display on the grid*/
+    public ScrollPane makeGridScrollPane(List<ImageView> contentList);
 
-    public ScrollPane makeListScrollPane(List<Level> levelList);
+    /** Creates a scrollable Pane that displays its contents in a list
+     *  @param dataList contains the {@code Data} to display */
+    public ScrollPane makeListScrollPane(List<Data> dataList);
 
+    /** Draws a {@code Level} to the specified target
+     *  @param root The target {@code Group} to draw to
+     *  @param level The {@code Level} to draw */
     public void drawStage(Group root, Level level);
 
+    /** Creates an editable {@code TextField}
+     *  @param text The default text to display in the {@code TextField}*/
     public TextField makeTextField(String text);
 
     public Slider makeSlider(Double field);
