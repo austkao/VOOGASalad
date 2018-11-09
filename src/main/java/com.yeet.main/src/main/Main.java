2817253222e757c78600657e6d00ae45a30e0340
Ben Xu <benjamin.xu@duke.edu>
2018-11-09 09:48:13 -0500

    added javafx application functionality to main module

--- src/main/java/com.yeet.main/src/main/Main.java
+++ src/main/java/com.yeet.main/src/main/Main.java
@@ -1,20 +1,25 @@
 package main;
 
-
 import console.external.Console;
+import javafx.application.Application;
+import javafx.stage.Stage;
 import messenger.external.MessageBusFactory;
 import player.external.Player;
 
-public class Main {
+public class Main extends Application {
 
     static Console myConsole;
 
     public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
         Player player = new Player();
         myConsole = new Console();
         MessageBusFactory.getEventBus().register(player);
         MessageBusFactory.getEventBus().register(myConsole);
         player.doSomething();
-
     }
 }
--- src/main/java/com.yeet.main/src/module-info.java
+++ src/main/java/com.yeet.main/src/module-info.java
@@ -1,7 +1,13 @@
 module com.yeet.main {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+
     requires com.yeet.messenger;
     requires com.google.common;
     requires com.yeet.player;
     requires com.yeet.editor;
     requires com.yeet.console;
+
+    exports main;
 }
