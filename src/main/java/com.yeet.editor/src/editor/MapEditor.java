903cc68ecb2418f8a46535a36ee687f30099d167
Ben Xu <benjamin.xu@duke.edu>
2018-11-11 11:09:05 -0500

    temporary compiler error fix to allow program testing

--- src/main/java/com.yeet.editor/src/editor/MapEditor.java
+++ src/main/java/com.yeet.editor/src/editor/MapEditor.java
@@ -2,26 +2,21 @@ package editor;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+
 import javafx.scene.image.Image;
-import renderer.external.*;
 import renderer.external.Structures.Level;
 
-public class MapEditor implements Renderer{
+public class MapEditor{
 
     private Level myLevel;
     private Image image;
     private Scene myScene;
 
+    // this class shouldn't implement Renderer, it should use the Editor's instance of RenderSystem
     public MapEditor(){
         myLevel = new Level(image);
         Group root = new Group();
         myScene = new Scene(root);
-    }
-
-    @Override
-    public Button makeImageButton(Image image, Double x, Double y) {
-        return null;
     }
 
 }
