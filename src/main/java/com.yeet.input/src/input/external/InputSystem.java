d41a22511eb78ece02cae52d1f5db26d14626211
josesanmartin <js665@duke.edu>
2018-11-11 17:10:25 -0500

    Added a queue to inputsystem

--- src/main/java/com.yeet.input/src/input/external/InputSystem.java
+++ src/main/java/com.yeet.input/src/input/external/InputSystem.java
@@ -6,14 +6,20 @@ import input.Internal.Parser;
 import messenger.external.EventBusFactory;
 import messenger.external.TestSuccesfulEvent;
 
+import java.awt.event.KeyEvent;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class InputSystem {
 
     EventBus myMessageBus;
     Parser myParser;
+    Queue<KeyEvent> CommandHolder;
 
     public InputSystem(){
         myMessageBus = EventBusFactory.getEventBus();
         myParser = new Parser();
+        CommandHolder = new LinkedList<>();
     }
 
     /*
@@ -33,6 +39,14 @@ public class InputSystem {
     public  void printMessage(TestSuccesfulEvent testSuccessfulEvent){
         // Simulate sending reciept
         System.out.println("Player: Test successful!");
+    }
+
+    /*
+     / The other subcribe event that listens for Key Inputs.
+     */
+    @Subscribe
+    public void getKey(InputEvent inputEvent){
+
     }
 
 }
