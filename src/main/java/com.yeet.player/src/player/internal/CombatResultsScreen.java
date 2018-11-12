7e2848bbc2e74c1b232e423c47c1196e82fc8553
Ben Xu <benjamin.xu@duke.edu>
2018-11-12 15:05:11 -0500

    added basic framework for game player screens

---
+++ src/main/java/com.yeet.player/src/player/internal/CombatResultsScreen.java
@@ -0,0 +1,14 @@
+package player.internal;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+/** Displays the winner of the combat as well as other related statistics and information
+ *  @author bpx
+ */
+public class CombatResultsScreen extends Scene {
+
+    public CombatResultsScreen(Parent root) {
+        super(root);
+    }
+}
---
+++ src/main/java/com.yeet.player/src/player/internal/MainMenuScreen.java
@@ -0,0 +1,15 @@
+package player.internal;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+/** Central hub for access to all game functionality
+ *  @author bpx
+ */
+public class MainMenuScreen extends Scene {
+
+    public MainMenuScreen(Parent root) {
+        super(root);
+    }
+
+}
