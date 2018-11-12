dca5599690f15d340c4f63ef122e33bdf947ad90
Scott McConnell <skm44@duke.edu>
2018-11-11 20:27:25 -0500

    Removed unnecesary direction property of X/YForce

--- src/main/java/com.yeet.physics/src/physics/external/XForce.java
+++ src/main/java/com.yeet.physics/src/physics/external/XForce.java
@@ -2,8 +2,7 @@ package physics.external;
 
 public class XForce {
 
-    public XForce(double mag, int dir) {
-        double magnitude = mag;
-        int direction = dir; // LEFT: -1, RIGHT: 1
+    public XForce(double mag) {
+        double magnitude = mag; // NEG: LEFT, POS: RIGHT
     }
 }
--- src/main/java/com.yeet.physics/src/physics/external/YForce.java
+++ src/main/java/com.yeet.physics/src/physics/external/YForce.java
@@ -2,8 +2,7 @@ package physics.external;
 
 public class YForce {
 
-    public YForce(double mag, int dir) {
-        double magnitude = mag;
-        int direction = dir; // UP: -1, DOWN: 1
+    public YForce(double mag) {
+        double magnitude = mag; // NEG: UP, POS: DOWN
     }
 }
