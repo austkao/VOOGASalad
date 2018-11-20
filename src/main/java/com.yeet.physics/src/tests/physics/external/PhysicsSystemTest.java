8a13c8ae39a2e047aebdd8d5fd0be2ad59f494f7
Scott McConnell <skm44@duke.edu>
2018-11-20 16:20:13 -0500

    Changed constructor of PhysicsSystem to only take in GravityAcceleration and GravityDirection, added an addPhysicsBodies(int) method

--- src/main/java/com.yeet.physics/src/tests/physics/external/PhysicsSystemTest.java
+++ src/main/java/com.yeet.physics/src/tests/physics/external/PhysicsSystemTest.java
@@ -9,34 +9,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PhysicsSystemTest {
 
-    public static final int defaultNumBodies = 50;
     public static final double defaultGravityAcceleration = 9.8;
     public static final double defaultGravityDirection = PI/2;
 
     @Test
     void createPhysicsBodies() {
-        PhysicsSystem testSystem = new PhysicsSystem(defaultNumBodies, defaultGravityAcceleration, defaultGravityDirection);
-        List<PhysicsBody> testBodies = testSystem.createPhysicsBodies(10);
+        PhysicsSystem testSystem = new PhysicsSystem(defaultGravityAcceleration, defaultGravityDirection);
+        testSystem.addPhysicsBodies(10);
         double expected = 0;
 
         for (int i = 0; i < 10; i++){
-            assertEquals(expected, testBodies.get(i).getAcceleration().getMagnitude());
-            assertEquals(expected, testBodies.get(i).getAcceleration().getDirection());
-            assertEquals(expected, testBodies.get(i).getVelocity().getMagnitude());
-            assertEquals(expected, testBodies.get(i).getVelocity().getDirection());
+            assertEquals(expected, testSystem.getBodies().get(i).getAcceleration().getMagnitude());
+            assertEquals(expected, testSystem.getBodies().get(i).getAcceleration().getDirection());
+            assertEquals(expected, testSystem.getBodies().get(i).getVelocity().getMagnitude());
+            assertEquals(expected, testSystem.getBodies().get(i).getVelocity().getDirection());
         }
     }
 
     @Test
     void applyGravity() {
-        PhysicsSystem testSystem = new PhysicsSystem(defaultNumBodies, defaultGravityAcceleration, defaultGravityDirection);
-        List<PhysicsBody> testBodies = testSystem.createPhysicsBodies(10);
+        PhysicsSystem testSystem = new PhysicsSystem(defaultGravityAcceleration, defaultGravityDirection);
+        testSystem.addPhysicsBodies(10);
         double expectedAccMag = 9.8;
         double expectedVelMag = 9.8*0.125;
         double expectedDir = PI/2;
 
-        testSystem.applyGravity(testBodies);
-        for (PhysicsBody b : testBodies) {
+        testSystem.applyGravity(testSystem.getBodies());
+        for (PhysicsBody b : testSystem.getBodies()) {
             assertEquals(expectedAccMag, b.getAcceleration().getMagnitude());
             assertEquals(expectedVelMag, b.getVelocity().getMagnitude());
             assertEquals(expectedDir, b.getAcceleration().getDirection());
