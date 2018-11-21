cf09c2f2849e6370d9d7c987665f8fd33491be1b
Scott McConnell <skm44@duke.edu>
2018-11-20 22:19:05 -0500

    Merge confict fixed

--- src/main/java/com.yeet.physics/src/physics/external/PhysicsSystem.java
+++ src/main/java/com.yeet.physics/src/physics/external/PhysicsSystem.java
@@ -7,7 +7,6 @@ import java.util.List;
 public class PhysicsSystem {
 
 
-<<<<<<< HEAD
     public static final double defaultMass = 50;
     List<PhysicsBody> bodies = new ArrayList<>();
     private double gravityAcceleration;
@@ -40,9 +39,5 @@ public class PhysicsSystem {
 
     List<PhysicsBody> getBodies() {
         return this.bodies;
-=======
-    public void update(){
-
->>>>>>> 3a63c53e08738dca3a6ef60ba4ac594f87251060
     }
 }
