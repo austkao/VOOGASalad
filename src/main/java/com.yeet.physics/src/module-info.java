cba8c58dffb999c3357d585a07106f4b8f261f3b
Ben Xu <benjamin.xu@duke.edu>
2018-11-27 17:51:04 -0500

    re-added junit dependency to physics module info

--- src/main/java/com.yeet.physics/src/module-info.java
+++ src/main/java/com.yeet.physics/src/module-info.java
@@ -1,6 +1,6 @@
 module com.yeet.physics {
     exports physics.external;
-    //requires org.junit.jupiter.api;
+    requires org.junit.jupiter.api;
     requires com.yeet.messenger;
     requires java.desktop;
     requires com.google.common;
