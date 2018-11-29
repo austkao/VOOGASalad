773bff0513f3d3a30578d493b24f530f5459d2db
josesanmartin <js665@duke.edu>
2018-11-28 22:57:36 -0500

    Added game start event to audio system

--- src/main/java/com.yeet.input/src/input/external/InputSystem.java
+++ src/main/java/com.yeet.input/src/input/external/InputSystem.java
@@ -72,8 +72,8 @@ public class InputSystem {
             myMessageBus.post(keyEvent);
 
             //TESTING: Also post an action event
-            ActionEvent ae = new ActionEvent(action, "Attack");
-            myMessageBus.post(ae);
+            //ActionEvent ae = new ActionEvent(action, "Attack");
+            //myMessageBus.post(ae);
         }
 
     }
