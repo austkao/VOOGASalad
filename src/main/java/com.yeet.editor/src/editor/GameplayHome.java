48580306938fcf76b051ae76421f2c36f4d341cc
austkao <nitsuaoak@outlook.com>
2018-11-28 22:37:30 -0500

    Trying to resolve file naming issue

--- src/main/java/com.yeet.editor/src/editor/GameplayHome.java
+++ src/main/java/com.yeet.editor/src/editor/GameplayHome.java
@@ -3,18 +3,19 @@ package editor;
 import javafx.scene.Group;
 
 public class GameplayHome extends EditorHome{
+
     public GameplayHome(Group root, EditorManager em){
         super(root,em);
         setEditor();
     }
 
-    public void setEditor(){
+    @Override
+    public void setEditor() {
         myEditor = new GameplayEditor(new Group(),em);
         myEditor.createBack(this);
     }
 
     public String toString(){
-        return "Gameplay Home";
+        return "Game Home";
     }
 }
-
