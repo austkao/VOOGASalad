6a59db9617bc7ae78383ff3cf2d4dfa30f6c45a3
Rahul Ramesh <rahul.ramesh@duke.edu>
2018-11-28 22:54:57 -0500

    working on animation playing

--- src/main/java/com.yeet.editor/src/editor/GameplayHome.java
+++ src/main/java/com.yeet.editor/src/editor/GameplayHome.java
@@ -2,19 +2,20 @@ package editor;
 
 import javafx.scene.Group;
 
-public class GameplayHome extends EditorHome{
-    public GameplayHome(Group root, EditorManager em){
+public class GamePlayHome extends EditorHome{
+
+    public GamePlayHome(Group root, EditorManager em){
         super(root,em);
         setEditor();
     }
 
-    public void setEditor(){
+    @Override
+    public void setEditor() {
         myEditor = new GameplayEditor(new Group(),em);
         myEditor.createBack(this);
     }
 
     public String toString(){
-        return "Gameplay Home";
+        return "Game Home";
     }
 }
-
