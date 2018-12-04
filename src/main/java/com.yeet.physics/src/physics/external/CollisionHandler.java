4c0418b68160f578c80ae1df717c8409ef5b122b
Scott McConnell <skm44@duke.edu>
2018-12-04 13:17:20 -0500

    Removed duplicate body attack collision handling

--- src/main/java/com.yeet.physics/src/physics/external/CollisionHandler.java
+++ src/main/java/com.yeet.physics/src/physics/external/CollisionHandler.java
@@ -28,14 +28,6 @@ public class CollisionHandler {
                 collisions.add(one.getId(), two.getId());
                 attackCollisions.add(collisions);
             }
-            // attack+body
-            if(one.isPhysicsAttack() && two.isPhysicsBody()){
-                PhysicsVector force = new PhysicsVector(defaultAttackMagnitude, one.getDirection());
-                two.addCurrentForce(force);
-                List<Integer> collisions = new ArrayList<>();
-                collisions.add(two.getId(), one.getId());
-                attackCollisions.add(collisions);
-            }
             // body+ground
             if(one.isPhysicsBody() && two.isPhysicsGround()){
                 one.setGrounded(true);
