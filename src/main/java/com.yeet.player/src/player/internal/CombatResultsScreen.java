2a23e6a92944ffa8251cf082c788996d7a0e0308
thequantumpotato <thequantumpotato@gmail.com>
2018-12-04 21:22:13 -0500

    started work on combatresultsscreen

--- src/main/java/com.yeet.player/src/player/internal/CombatResultsScreen.java
+++ src/main/java/com.yeet.player/src/player/internal/CombatResultsScreen.java
@@ -1,7 +1,14 @@
 package player.internal;
 
+import javafx.geometry.Pos;
 import javafx.scene.Group;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import renderer.external.Renderer;
+
+import javax.swing.text.html.ImageView;
 
 /** Displays the winner of the combat as well as other related statistics and information
  *  @author bpx
@@ -10,5 +17,14 @@ public class CombatResultsScreen extends Screen {
 
     public CombatResultsScreen(Group root, Renderer renderer) {
         super(root, renderer);
+        VBox mainContainer = new VBox();
+        Rectangle topSpacer = new Rectangle(1280,20, Color.TRANSPARENT);
+        StackPane winnerBanner = new StackPane();
+        winnerBanner.setPrefSize(1280.0,200.0);
+        winnerBanner.setAlignment(Pos.CENTER);
+        ImageView winnerBannerImage;
+
+        mainContainer.getChildren().addAll(topSpacer);
+        this.getMyRoot().getChildren().addAll(mainContainer);
     }
 }
