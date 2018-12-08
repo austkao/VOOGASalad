176f619354a76612e0646ff445c10a4724f552cd
Ben Xu <benjamin.xu@duke.edu>
2018-12-08 00:30:08 -0500

    feature: added message bar to main menu to provide tooltips about the buttons

--- src/main/java/com.yeet.player/src/player/internal/MainMenuScreen.java
+++ src/main/java/com.yeet.player/src/player/internal/MainMenuScreen.java
@@ -6,6 +6,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import player.internal.Elements.MessageBar;
 import renderer.external.Renderer;
 
 import java.io.File;
@@ -15,7 +17,16 @@ import java.io.File;
  */
 public class MainMenuScreen extends Screen {
 
+    public static final String SMASH_TITLE = "SMASH";
+    public static final String SMASH_MSG = "Jump right into a brawl with up to 4 players!";
+    public static final String SETTINGS_TITLE = "SETTINGS";
+    public static final String SETTINGS_MSG = "Adjust the game settings to your liking!";
+    public static final String QUIT_TITLE = "QUIT";
+    public static final String QUIT_MSG = "Aw, leaving so soon?";
+    public static final String DEFAULT_TITLE = "Message";
+    public static final String DEFAULT_MSG = "Welcome to Yeet Bros. Brawl!";
     private MediaPlayer selectSE;
+    private MessageBar myMessageBar;
 
     public MainMenuScreen(Group root, Renderer renderer, SceneSwitch smashSceneSwitch, SceneSwitch quitSceneSwitch) {
         super(root, renderer);
@@ -23,36 +34,45 @@ public class MainMenuScreen extends Screen {
         ImageView background = new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream("mainmenu_bg.png")));
         background.setFitHeight(800.0);
         background.setFitWidth(1280.0);
-        ImageView smashButton = makeButton("smash_button.png",880.0,523.0,74.0,85.0,event -> {
+        ImageView smashButton = makeButton("smash_button.png", SMASH_TITLE, SMASH_MSG,880.0,523.0,74.0,85.0, event -> {
             selectSE.play();
             selectSE.setOnEndOfMedia(() -> {
                 smashSceneSwitch.switchScene();
                 selectSE.stop();
             });
         });
-        ImageView settingsButton = makeButton("settings_button.png",610.0,146.0,649.0,254.0,event -> {
+        ImageView settingsButton = makeButton("settings_button.png", SETTINGS_TITLE, SETTINGS_MSG,610.0,146.0,649.0,254.0, event -> {
+
             //TODO: implement settings scene
             selectSE.play();
         });
-        ImageView quitButton = makeButton("quit_button.png",610.0,132.0,649.0,436.0,event -> quitSceneSwitch.switchScene());
-        super.getMyRoot().getChildren().addAll(background,smashButton,settingsButton,quitButton);
+        ImageView quitButton = makeButton("quit_button.png",QUIT_TITLE, QUIT_MSG,610.0,132.0,649.0,436.0, event -> quitSceneSwitch.switchScene());
+        myMessageBar = new MessageBar(this.getMyRenderer().makeText(DEFAULT_TITLE,true,50, Color.WHITE,0.0,0.0),
+                this.getMyRenderer().makeText(DEFAULT_MSG,false,30, Color.BLACK,0.0,0.0),
+                174.0,676.0);
+        super.getMyRoot().getChildren().addAll(background,myMessageBar,smashButton,settingsButton,quitButton);
     }
 
-    private ImageView makeButton(String fileName, double width, double height, double x, double y, EventHandler clickHandler) {
-        ImageView smashButton = new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream(fileName)));
-        smashButton.setFitWidth(width);
-        smashButton.setFitHeight(height);
-        smashButton.setX(x);
-        smashButton.setY(y);
-        smashButton.setOnMouseEntered(event -> {
-            smashButton.setScaleY(1.1);
-            smashButton.setScaleX(1.1);
+    private ImageView makeButton(String fileName, String messageTitle, String messageContent, double width, double height, double x, double y, EventHandler clickHandler) {
+        ImageView button = new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream(fileName)));
+        button.setFitWidth(width);
+        button.setFitHeight(height);
+        button.setX(x);
+        button.setY(y);
+        button.setOnMouseEntered(event -> {
+            button.setScaleY(1.1);
+            button.setScaleX(1.1);
+            myMessageBar.setTitle(messageTitle);
+            myMessageBar.setMessage(messageContent);
+            myMessageBar.show();
+
         });
-        smashButton.setOnMouseExited(event -> {
-            smashButton.setScaleY(1.0);
-            smashButton.setScaleX(1.0);
+        button.setOnMouseExited(event -> {
+            button.setScaleY(1.0);
+            button.setScaleX(1.0);
+            myMessageBar.hide();
         });
-        smashButton.setOnMousePressed(clickHandler);
-        return smashButton;
+        button.setOnMousePressed(clickHandler);
+        return button;
     }
 }
