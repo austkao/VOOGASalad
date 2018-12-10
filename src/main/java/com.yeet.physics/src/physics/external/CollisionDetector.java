82e26e44030f9a554a4904a6eded4381f138bd70
Joshua France <joshua.france@duke.edu>
2018-12-10 00:08:59 -0500

    collision detection optimized to skip grounds

--- src/main/java/com.yeet.physics/src/physics/external/CollisionDetector.java
+++ src/main/java/com.yeet.physics/src/physics/external/CollisionDetector.java
@@ -19,16 +19,18 @@ CollisionDetector {
         List<Collision> collisions = new ArrayList<>();
         Map<Integer, PhysicsObject> currentBodies = new HashMap<>(bodies);
         for(PhysicsObject bod: currentBodies.values()){
-            for(PhysicsObject bod2: currentBodies.values()){
-                if(!bod.equals(bod2)){
-                    Intersection myInt = bod.getMyCoordinateBody().intersects(bod2.getMyCoordinateBody());
-                    Side s = myInt.getIntersector();
-                    if(!s.getMySide().equals("NONE")){
-                        List<PhysicsObject> colliders = new ArrayList<>();
-                        colliders.add(bod);
-                        colliders.add(bod2);
-                        Collision col = new Collision(colliders, s);
-                        collisions.add(col);
+            for(PhysicsObject bod2: currentBodies.values()) {
+                if (!(bod.isPhysicsGround() && bod2.isPhysicsGround())) {
+                    if (!bod.equals(bod2)) {
+                        Intersection myInt = bod.getMyCoordinateBody().intersects(bod2.getMyCoordinateBody());
+                        Side s = myInt.getIntersector();
+                        if (!s.getMySide().equals("NONE")) {
+                            List<PhysicsObject> colliders = new ArrayList<>();
+                            colliders.add(bod);
+                            colliders.add(bod2);
+                            Collision col = new Collision(colliders, s);
+                            collisions.add(col);
+                        }
                     }
                 }
             }
