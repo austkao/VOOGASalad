0a51939358c57ba32e5c07ee3b374a3b2594ba83
Ben Xu <benjamin.xu@duke.edu>
2018-12-10 02:33:37 -0500

    feature update: made event implement serializable

--- src/main/java/com.yeet.messenger/src/messenger/external/Event.java
+++ src/main/java/com.yeet.messenger/src/messenger/external/Event.java
@@ -1,7 +1,9 @@
 package messenger.external;
 
+import java.io.Serializable;
+
 /** Superclass for anything that will be sent through the {@code EventBus}*/
-public abstract class Event {
+public abstract class Event implements Serializable {
 
     /** Return the name of the event*/
     public abstract String getName();
