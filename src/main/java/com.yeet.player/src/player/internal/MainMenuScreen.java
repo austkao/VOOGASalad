b26a1e0abd7bfed04a5c73633e56428e8e6e35da
Ben Xu <benjamin.xu@duke.edu>
2018-12-09 19:27:48 -0500

    feature update: visual updates to settings screen

--- src/main/java/com.yeet.player/src/player/internal/MainMenuScreen.java
+++ src/main/java/com.yeet.player/src/player/internal/MainMenuScreen.java
@@ -25,38 +25,42 @@ public class MainMenuScreen extends Screen {
     public static final String QUIT_MSG = "Aw, leaving so soon?";
     public static final String DEFAULT_TITLE = "Message";
     public static final String DEFAULT_MSG = "Welcome to Yeet Bros. Brawl!";
+    public static final int MESSAGEBAR_TITLE_FONTSIZE = 50;
+    public static final int MESSAGEBAR_MSG_FONTSIZE = 30;
+    public static final double MESSAGEBAR_X = 174.0;
+    public static final double MESSAGEBAR_Y = 676.0;
     private MediaPlayer selectSE;
     private MessageBar myMessageBar;
 
     public MainMenuScreen(Group root, Renderer renderer, SceneSwitch smashSceneSwitch, SceneSwitch quitSceneSwitch, SceneSwitch settingsSceneSwitch) {
         super(root, renderer);
+        myMessageBar = new MessageBar(this.getMyRenderer().makeText(DEFAULT_TITLE,true, MESSAGEBAR_TITLE_FONTSIZE, Color.WHITE,0.0,0.0),
+                this.getMyRenderer().makeText(DEFAULT_MSG,false, MESSAGEBAR_MSG_FONTSIZE, Color.BLACK,0.0,0.0),
+                MESSAGEBAR_X, MESSAGEBAR_Y);
         selectSE = new MediaPlayer(new Media(new File("src/main/java/com.yeet.player/resources/select.mp3").toURI().toString()));
         ImageView background = new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream("mainmenu_bg.png")));
         background.setFitHeight(800.0);
         background.setFitWidth(1280.0);
-        ImageView smashButton = makeButton("smash_button.png", SMASH_TITLE, SMASH_MSG,880.0,523.0,74.0,85.0, event -> {
+        ImageView smashButton = makeButton(new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream("smash_button.png"))), SMASH_TITLE, SMASH_MSG,880.0,523.0,74.0,85.0, myMessageBar, event -> {
             selectSE.setOnEndOfMedia(() -> {
                 smashSceneSwitch.switchScene();
                 selectSE.stop();
             });
             selectSE.play();
         });
-        ImageView settingsButton = makeButton("settings_button.png", SETTINGS_TITLE, SETTINGS_MSG,610.0,146.0,649.0,254.0, event -> {
+        ImageView settingsButton = makeButton(new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream("settings_button.png"))), SETTINGS_TITLE, SETTINGS_MSG,610.0,146.0,649.0,254.0, myMessageBar, event -> {
             selectSE.setOnEndOfMedia(() -> {
                 settingsSceneSwitch.switchScene();
                 selectSE.stop();
             });
             selectSE.play();
         });
-        ImageView quitButton = makeButton("quit_button.png",QUIT_TITLE, QUIT_MSG,610.0,132.0,649.0,436.0, event -> quitSceneSwitch.switchScene());
-        myMessageBar = new MessageBar(this.getMyRenderer().makeText(DEFAULT_TITLE,true,50, Color.WHITE,0.0,0.0),
-                this.getMyRenderer().makeText(DEFAULT_MSG,false,30, Color.BLACK,0.0,0.0),
-                174.0,676.0);
+        ImageView quitButton = makeButton(new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream("quit_button.png"))),QUIT_TITLE, QUIT_MSG,610.0,132.0,649.0,436.0, myMessageBar, event -> quitSceneSwitch.switchScene());
         super.getMyRoot().getChildren().addAll(background,myMessageBar,smashButton,settingsButton,quitButton);
     }
 
-    private ImageView makeButton(String fileName, String messageTitle, String messageContent, double width, double height, double x, double y, EventHandler clickHandler) {
-        ImageView button = new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream(fileName)));
+    static ImageView makeButton(ImageView buttonImage, String messageTitle, String messageContent, double width, double height, double x, double y, MessageBar messageBar, EventHandler clickHandler) {
+        ImageView button = buttonImage;
         button.setFitWidth(width);
         button.setFitHeight(height);
         button.setX(x);
@@ -64,15 +68,15 @@ public class MainMenuScreen extends Screen {
         button.setOnMouseEntered(event -> {
             button.setScaleY(1.1);
             button.setScaleX(1.1);
-            myMessageBar.setTitle(messageTitle);
-            myMessageBar.setMessage(messageContent);
-            myMessageBar.show();
+            messageBar.setTitle(messageTitle);
+            messageBar.setMessage(messageContent);
+            messageBar.show();
 
         });
         button.setOnMouseExited(event -> {
             button.setScaleY(1.0);
             button.setScaleX(1.0);
-            myMessageBar.hide();
+            messageBar.hide();
         });
         button.setOnMousePressed(clickHandler);
         return button;
