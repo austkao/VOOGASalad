43cb18252cddbfef80f69b27116841cac6d7ea3d
Ben Xu <benjamin.xu@duke.edu>
2018-12-10 03:19:43 -0500

    feature: added basic functionality to Replay

--- src/main/java/com.yeet.player/src/player/internal/Elements/Replay.java
+++ src/main/java/com.yeet.player/src/player/internal/Elements/Replay.java
@@ -1,18 +1,42 @@
 package player.internal.Elements;
 
+import messenger.external.Event;
+
 import java.io.Serializable;
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 /** Created and read by the recorder, can be saved to file to reproduce a sequence of events during a combat
  *  @author bpx
  */
 public class Replay implements Serializable {
 
-    private LinkedList eventList;
+    private int index;
+    private ArrayList<Event> eventList;
+    private int length;
 
     public Replay(){
-        eventList = new LinkedList();
+        index = -1;
+        length = 0;
+        eventList = new ArrayList<>();
+    }
 
+    /** Add an {@code Event} to the replay
+     *  @param frame Any kind of {@code Event}
+     */
+    public void add(Event frame){
+        eventList.add(frame);
+        length++;
+    }
+
+    /** Returns the next {@code Event} in the {@code Replay}, throws {@code EndOfReplayException} if at end*/
+    public Event next() throws EndOfReplayException{
+        if(index<length){
+            index++;
+            return eventList.get(index);
+        }
+        else{
+            throw new EndOfReplayException();
+        }
     }
 
 }
