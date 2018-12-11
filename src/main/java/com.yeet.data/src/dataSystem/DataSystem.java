79da69693ff1cf96fa9fc3157f290d88c53e80ad
austkao <nitsuaoak@outlook.com>
2018-12-10 22:39:36 -0500

    Refactored data module

--- src/main/java/com.yeet.data/src/dataSystem/DataSystem.java
+++ src/main/java/com.yeet.data/src/dataSystem/DataSystem.java
@@ -2,6 +2,7 @@ package dataSystem;
 
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
+import dataSubsystem.FileSystemHandler;
 import javafx.event.Event;
 import messenger.external.*;
 import org.w3c.dom.Attr;
@@ -16,28 +17,35 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * A system module created at runtime, much like Player and CombatSystem
+ * Receives messages from the message bus pertaining to Events that deal with the files in the file system
+ * @author ak457
+ */
 public class DataSystem {
     private EventBus myEB;
     private FilePath myFP;
+    private FileSystemHandler myFileSystem;
 
     public DataSystem() {
         myEB = EventBusFactory.getEventBus();
         myEB.register(this);
+        myFileSystem = new FileSystemHandler();
     }
 
     @Subscribe
     public void createInitialGameFiles(CreateGameEvent event) {
         File defaultFile = event.getDirectory();
         defaultFile.mkdir();
-        createDirectory(defaultFile.getPath() + myFP.STAGEPATH.getPath());
-        createDirectory(defaultFile.getPath() + myFP.CHARACTERPATH.getPath());
-        createDirectory(defaultFile.getPath() + myFP.DATAPATH.getPath());
-        createDirectory(defaultFile.getPath() + myFP.BACKGROUNDPATH.getPath());
-        createDirectory(defaultFile.getPath() + myFP.BGMPATH.getPath());
-        createDirectory(defaultFile.getPath() + myFP.TILEPATH.getPath());
-        createDirectory(defaultFile.getPath() + myFP.MODE.getPath());
-        createDirectory(defaultFile.getPath() + myFP.TIME.getPath());
-        createDirectory(defaultFile.getPath() + myFP.STOCK.getPath());
+        myFileSystem.createDirectory(defaultFile.getPath() + myFP.STAGEPATH.getPath());
+        myFileSystem.createDirectory(defaultFile.getPath() + myFP.CHARACTERPATH.getPath());
+        myFileSystem.createDirectory(defaultFile.getPath() + myFP.DATAPATH.getPath());
+        myFileSystem.createDirectory(defaultFile.getPath() + myFP.BACKGROUNDPATH.getPath());
+        myFileSystem.createDirectory(defaultFile.getPath() + myFP.BGMPATH.getPath());
+        myFileSystem.createDirectory(defaultFile.getPath() + myFP.TILEPATH.getPath());
+        myFileSystem.createDirectory(defaultFile.getPath() + myFP.MODE.getPath());
+        myFileSystem.createDirectory(defaultFile.getPath() + myFP.TIME.getPath());
+        myFileSystem.createDirectory(defaultFile.getPath() + myFP.STOCK.getPath());
     }
 
     @Subscribe
@@ -56,8 +64,8 @@ public class DataSystem {
             System.out.println("Could not create stage file");
         }
     }
-
-    public void overwriteGameFile(File xmlFile) {
+    @Deprecated
+    private void overwriteGameFile(File xmlFile) {
         XMLParser parser = new XMLParser(xmlFile);
         Document xmlDoc = parser.createDocument(xmlFile);
         NodeList voogaNodes = xmlDoc.getElementsByTagName("VOOGASalad");
@@ -82,45 +90,17 @@ public class DataSystem {
     public void createCharacterFiles(CreateCharacterEvent event) {
         File characterDirectory = event.getDirectory();
         characterDirectory.mkdir();
-        createDirectory(characterDirectory.getPath()+ myFP.ATTACK.getPath());
-        createDirectory(characterDirectory.getPath()+ myFP.SOUND.getPath());
-        createDirectory(characterDirectory.getPath()+ myFP.SPRITE.getPath());
-        createFile(characterDirectory.getPath() + myFP.CHARACTERPROPERTIES.getPath());
-        createFile(characterDirectory.getPath() + myFP.ATTACKPROPERTIES.getPath());
-        createFile(characterDirectory.getPath() + myFP.SOUNDPROPERTIES.getPath());
-        createFile(characterDirectory.getPath() + myFP.SPRITEPROPERTIES.getPath());
+        myFileSystem.createDirectory(characterDirectory.getPath()+ myFP.ATTACK.getPath());
+        myFileSystem.createDirectory(characterDirectory.getPath()+ myFP.SOUND.getPath());
+        myFileSystem.createDirectory(characterDirectory.getPath()+ myFP.SPRITE.getPath());
+        myFileSystem.createFile(characterDirectory.getPath() + myFP.CHARACTERPROPERTIES.getPath());
+        myFileSystem.createFile(characterDirectory.getPath() + myFP.ATTACKPROPERTIES.getPath());
+        myFileSystem.createFile(characterDirectory.getPath() + myFP.SOUNDPROPERTIES.getPath());
+        myFileSystem.createFile(characterDirectory.getPath() + myFP.SPRITEPROPERTIES.getPath());
     }
 
     @Subscribe
     public void deleteDirectory(DeleteDirectoryEvent event) {
-        File directory = event.getDirectory();
-        while(directory.exists()) {
-            deleteFile(directory);
-        }
-    }
-
-    private void createDirectory(String path) {
-        File directory = new File(path);
-        directory.mkdir();
-    }
-
-    private void createFile(String path) {
-        File file = new File(path);
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-
-        }
-    }
-
-    private void deleteFile(File file) {
-        File[] files = file.listFiles();
-        if(files == null || files.length == 0) {
-            file.delete();
-        } else {
-            for(File f : files) {
-                deleteFile(f);
-            }
-        }
+        myFileSystem.deleteFile(event.getDirectory());
     }
 }
