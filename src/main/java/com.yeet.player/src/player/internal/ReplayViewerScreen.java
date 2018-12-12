d894246a39c14278a81f549a3e8c090b95c91034
Ben Xu <benjamin.xu@duke.edu>
2018-12-11 20:59:53 -0500

    created replay viewer screen

---
+++ src/main/java/com.yeet.player/src/player/internal/ReplayViewerScreen.java
@@ -0,0 +1,13 @@
+package player.internal;
+
+import javafx.scene.Group;
+import renderer.external.Renderer;
+
+/** Screen for viewing the replay
+ *  @author bpx
+ */
+public class ReplayViewerScreen extends Screen {
+    public ReplayViewerScreen(Group root, Renderer renderer) {
+        super(root, renderer);
+    }
+}
